76d6ccdc87e7ba5f53b40e8759d9daa595a60fcf
Axel Isouard <axel@isouard.fr>
2017-02-18 18:22:38 +0100

    Add DefinitelyTyped index file

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for node-webrtc
+// Project: https://github.com/aisouard/node-webrtc/
+// Definitions by: Axel Isouard <axel@isouard.fr>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference path="lib/RTCSessionDescription.d.ts" />
