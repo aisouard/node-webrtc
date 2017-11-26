c22586a47b7878e3eb4d870a8e2b8d85840e85a9
Axel Isouard <axel@isouard.fr>
2017-11-26 14:37:10 +0100

    Update package.json, increase RTCCertificate expire check test

--- test/RTCCertificate.test.js
+++ test/RTCCertificate.test.js
@@ -51,7 +51,7 @@ describe('RTCCertificate', () => {
           Math.floor((now + (1000 * 60 * 60 * 24 * 30)) / 1000) * 1000;
 
         assert.closeTo(Math.floor(certificate.expires / 1000) * 1000,
-          expected, 1000);
+          expected, 10000);
       });
     });
 
